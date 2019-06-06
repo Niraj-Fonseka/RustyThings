e7d7909b8d78556ec8b2d0c5d4982b636803da45
MNF4691 <m_n_fonseka@homedepot.com>
2019-06-06 14:09:09 -0500

    continuing guessing game

--- TheRustProgrammingLanguageBook/Chapter_02/guessing_game/src/main.rs
+++ TheRustProgrammingLanguageBook/Chapter_02/guessing_game/src/main.rs
@@ -1,27 +1,14 @@
 use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number !"); 
-    
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is : {} ", secret_number);
+    println!("Guess the number!");
 
     println!("Please input your guess.");
 
-    let mut guess = String::new(); //mutable 
-   
+    let mut guess = String::new();
+
     io::stdin().read_line(&mut guess)
-	.expect("Failed to read line");
+        .expect("Failed to read line");
 
-    println!("You Guessed : {}", guess);
-
-
-    match guess.cmp(&secret_number) {
-	Ordering::Less => println!("Too Small !"),
-	Ordering::Greater => println!("Too Big !"),
-	Ordering::Equal => println!("You win !"),
-   }
+    println!("You guessed: {}", guess);
 }
